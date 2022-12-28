8e338e41c3443c2668dd22a261032b4345949150
harryuan65 <harryuan.65@gmail.com>
2022-12-28 14:16:00 +0800

    feat: add styles

--- clients/react_cra/src/App.tsx
+++ clients/react_cra/src/App.tsx
@@ -1,18 +1,36 @@
 import React, { useState } from 'react';
-import './App.css';
+import classes from './App.module.css';
 // Local test
 import { ToDoItems } from './constants';
+import { ToDoItemStatus } from './types';
 
 function App() {
   const [items, setItems] = useState(ToDoItems);
   return (
-    <div>
-      {items.map((item, i) => (
-        <div key={i}>
-          <input type="checkbox" />
-          <span>{item.title}</span>
+    <div className={classes.Container}>
+      <div className={classes.filterBar}>
+        <input type="text" className={classes.Search} />
+        <div className={classes.statuses}>
+          <span className={classes.All}>All</span>
+          <span className={classes.Active}>Active</span>
+          <span className={classes.Done}>Done</span>
         </div>
-      ))}
+      </div>
+      <div className={classes.Items}>
+        {items.map((item, i) => (
+          <div className={classes.Item} key={i}>
+            <input
+              type="checkbox"
+              checked={item.status === ToDoItemStatus.DONE}
+            />
+            <span>{item.title}</span>
+          </div>
+        ))}
+      </div>
+      <div className={classes.additionBar}>
+        <input type="text" placeholder="Something on your mind?" />
+        <button>ADD</button>
+      </div>
     </div>
   );
 }

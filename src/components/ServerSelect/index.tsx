5a5bc1d17da135d28936a981f084bf42cfc0fdee
harryuan65 <harryuan.65@gmail.com>
2023-04-06 22:41:59 +0800

    feat: update server select styles

--- src/components/ServerSelect/index.tsx
+++ src/components/ServerSelect/index.tsx
@@ -33,7 +33,10 @@ const ServerSelect = ({
         <input type="text" placeholder="PORT" value={port} onChange={onChangePort} />
         <button onClick={onSaveUpdateServerUrl}>Save</button>
       </p>
-      {usingServer && <span>Using Server: {serverUrl }</span>}
+      {usingServer && <div className={classes.serverSpecInfo}>
+        <p>The server must implement <a href="https://editor.swagger.io/?url=https://raw.githubusercontent.com/harryuan65/to-do-list-api-rails/main/swagger/v1/swagger.yaml" target="_blank" rel="noreferrer noopener">this spec</a>.</p>
+        <p>Using Server: {serverUrl}</p>
+      </div>}
     </div>
   );
 };
